e0905b065d009303e635a1deb9ade3d20238f4d3
Aspitrine <loquente62@gmail.com>
2023-11-06 09:27:45 +0100

    Récupération de l'id de l'article depuis les params

--- src/routes/Article/Article.tsx
+++ src/routes/Article/Article.tsx
@@ -1,5 +1,14 @@
+import { useParams } from 'react-router-dom';
+
 function Article() {
-  return <div>Mon Article</div>;
+  // Pour récupérer les paramètres de l'url, on utilise le hook useParams
+  const { id } = useParams();
+
+  if (!id) {
+    throw new Error("l'url ne contient pas d'id");
+  }
+
+  return <div>Mon Article {id}</div>;
 }
 
 export default Article;
